c547fcd8935affa8d3add71cfc78abc4e2673777
M Brynard <mbrynard@gmail.com>
2016-06-05 17:45:47 +0200

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+/*
+ * @Package Name : pure-coming-soon-freebies-106
+ *
+ * @Website : www.pixelaar.com
+ * @Author : Pixelaar LLC
+ */
+
+jQuery(function ($) {
+    'use strict';
+    // -------------------------------------------------------------
+    // Countdown
+    // -------------------------------------------------------------
+    (function () {
+
+        $("#countdowndate").countdown({
+            date: "30 september 2018 12:00:00",
+            format: "on"
+        });
+
+    }());
+
+
+}); // JQuery end
